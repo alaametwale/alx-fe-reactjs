f07e8a9cc7b610b19f741731ca05fc5b87e732b2
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-01-12 15:15:34 +0200

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,8 +1,14 @@
 function MainContent() {
   return (
-    <main style={{ padding: '20px', backgroundColor: '#f4f4f4' }}>
-      <p style={{ fontSize: '18px' }}>
-        Welcome to the main content section.
+    <main
+      style={{
+        padding: '20px',
+        backgroundColor: '#f4f4f4',
+        textAlign: 'center'
+      }}
+    >
+      <p style={{ fontSize: '18px', color: '#333' }}>
+        I love to visit New York, Paris, and Tokyo.
       </p>
     </main>
   );
