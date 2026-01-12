5ddf2871194e70c87c2b94b8c5ab1a57aff05a54
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-01-12 14:48:11 +0200

    Create Footer.jsx

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: '#222',
+        color: 'white',
+        textAlign: 'center',
+        padding: '10px'
+      }}
+    >
+      <p>© 2026 My React App</p>
+    </footer>
+  );
+}
+
+export default Footer;
