f1911b8327a7e06de99a4563bd7daa666113feee
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-01-26 00:56:18 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,9 @@
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   return (
