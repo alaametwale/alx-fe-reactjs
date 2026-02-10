bd8d79b1cc69c55f7599647051e50090f8b4c544
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-02-10 15:45:42 +0200

    Create App.jsx

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import RecipeDetail from "./components/RecipeDetail";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
