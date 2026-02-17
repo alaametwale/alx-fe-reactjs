4ba2cbadb583ad93882fbe16bcf425508b36f397
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-02-17 15:53:31 +0200

    Create App.jsx

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,18 @@
+import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/FormikForm";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Controlled Form</h1>
+      <RegistrationForm />
+
+      <hr />
+
+      <h1>Formik Form</h1>
+      <FormikForm />
+    </div>
+  );
+}
+
+export default App;
