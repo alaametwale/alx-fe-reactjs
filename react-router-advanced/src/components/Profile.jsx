81aa7d0d155e3d565a5965bfabd7c4340d97526f
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-02-17 16:49:28 +0200

    Create Profile.jsx

---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+
+function Profile() {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+
+      <nav>
+        <Link to="details">Profile Details</Link> |{" "}
+        <Link to="settings">Profile Settings</Link>
+      </nav>
+
+      {/* Nested routes will render here */}
+      <Outlet />
+    </div>
+  );
+}
+
+export default Profile;
