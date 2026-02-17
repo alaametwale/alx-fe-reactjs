ddc4e11cbcccba6c2a600d2c9fb07c70eb336c68
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-02-17 16:55:42 +0200

    Update ProtectedRoute.jsx

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,11 +1,19 @@
 import { Navigate } from "react-router-dom";
 
-const isAuthenticated = true; // Simulate authentication
+// Simulate a custom hook
+function useAuth() {
+  // يمكنك تعديلها لاحقًا للتحقق من تسجيل الدخول الحقيقي
+  const user = { loggedIn: true }; // true = مصرح له
+  return user && user.loggedIn;
+}
 
 function ProtectedRoute({ children }) {
+  const isAuthenticated = useAuth();
+
   if (!isAuthenticated) {
     return <Navigate to="/login" />;
   }
+
   return children;
 }
 
